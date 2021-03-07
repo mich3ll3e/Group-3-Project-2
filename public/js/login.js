e2968903700ff756256227047b1bc47a83c5250c
Laith Alwani <laithalwani@gmail.com>
2021-03-06 20:25:54 -0500

    added passport and session for login authentication and user sessions

---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $("#login-form").on("submit",function(e){
+        e.preventDefault();
+        const user = {
+            username:$("#username").val().trim(),
+            password:$("#password").val().trim()
+        }
+        loginUser(user);
+        $("#username").val("");
+        $("#password").val("")
+    });
+
+    function loginUser(user) {
+        
+    $.ajax("/api/login", {
+      type: "POST",
+      data: user
+    }).then(function() {
+        location.replace("/chat");
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
+});
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const session = require("express-session");
 const http = require("http");
 const socketio = require("socket.io");
 const formatMessage = require("./utils/messages");
+const passport = require("./config/passport");
+const isAuthenticated = require("./config/middleware/isAuthenticated");
 const db = require("./models");
 
 const app = express();
@@ -11,23 +14,28 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 const botName = "ChatBot";
+let username="username";
 
 io.on("connection", socket=>{
     //welcome current user
     socket.emit("message",formatMessage(botName, "welcome to chat"));
 
     //Bordcast when a user connects
-    socket.broadcast.emit("message", formatMessage(botName, "A User has joined the chat"));
+    socket.broadcast.emit("message", formatMessage(botName,`${username}has joined the chat` ));
 
     //when client disconnects
     socket.on("disconnect",()=>{
-        io.emit("message", formatMessage(botName, "A User has left the chat"));
+        io.emit("message", formatMessage(botName, `${username} has left the chat`));
     });
 
     //Listen for chat Message
@@ -40,8 +48,11 @@ io.on("connection", socket=>{
 app.get("/signup",(req,res)=>{
     res.render("signup");
 });
-app.get("/chat",(req,res)=>{
-    res.render("chat");
+app.get("/chat", isAuthenticated,(req,res)=>{
+    db.User.findAll({raw: true}).then(dbUsers=>{
+        res.render("chat",{users:dbUsers});
+    })
+    
 });
 app.get("/",(req,res)=>{
     res.render("index")
@@ -63,7 +74,27 @@ app.post("/api/signup",(req,res)=>{
       .catch(function(err) {
         res.status(401).json(err);
       });
-})
+});
+
+app.post("/api/login", passport.authenticate("local"),(req,res)=>{
+    console.log(req.user);
+    res.json(req.user);
+});
+
+app.get("/api/user_data", function(req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    } else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      username = req.user.username;
+      res.json({
+        username: req.user.username,
+        id: req.user.id
+      });
+    }
+  });
 
 const PORT = process.env.PORT || 8080
 
